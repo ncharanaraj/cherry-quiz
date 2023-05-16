ba60dd2fec99f7a1fde60a29a9547cc1eb7ac6c9
Charanaraj N <goudacharanraj760@gmail.com>
2023-05-16 16:11:17 +0530

    initial commit

---
+++ src/data/Questions.js
@@ -0,0 +1,26 @@
+export const QuestionsData = [
+  {
+    question: "Who invented JavaScript?",
+    options: [
+      { text: "Douglas Crockford" },
+      { text: "Sheryl Sandberg" },
+      { text: "Brendan Eich" },
+    ],
+    answer: "Brendan Eich",
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    options: [{ text: "Node.js" }, { text: "TypeScript" }, { text: "npm" }],
+    answer: "npm",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    options: [
+      { text: "Angular" },
+      { text: "jQuery" },
+      { text: "RequireJS" },
+      { text: "ESLint" },
+    ],
+    answer: "ESLint",
+  },
+];
